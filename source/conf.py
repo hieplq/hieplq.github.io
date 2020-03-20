3c14f7e7cd47ce49bee1f98d5dccddf9e7377dde
hieplq <hieplq@hasuvimex.vn>
2020-03-20 12:45:34 +0700

    theme sphinx13: change to theme like sphinx doc https://www.sphinx-doc.org/en/master/contents.html (not yet correct link and rebranch)

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,6 @@
 #
 # import os
 # import sys
-import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -32,7 +31,6 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
     'sphinx.ext.viewcode'
 ]
 
@@ -51,8 +49,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
-
+html_theme = "sphinx13"
+html_theme_path = ['_themes']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
